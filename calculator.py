a7949da888eef93da060a0b8a9ba73db75052c69
Moshiur Rahman Sabbir <moshiurrahman.datawizard@gmail.com>
2024-06-24 11:56:36 +0600

    make comment for better understanding

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 class Calculator(tk.Tk):
+    #main class for creating the calculator app
     def __init__(self):
         super().__init__()
         self.title("Calculator")
